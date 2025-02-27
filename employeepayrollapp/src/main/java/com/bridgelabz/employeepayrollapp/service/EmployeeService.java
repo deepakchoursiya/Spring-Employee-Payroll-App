47d487e97a7617c666049810710df59e2e7d133c
deepakchoursiya <chourasiadeepak2002@gmail.com>
2025-02-27 11:28:03 +0530

    Deepak Add. Implemented lombok @slf4j logging in Employee Service

--- employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeeService.java
+++ employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeeService.java
@@ -3,12 +3,14 @@ package com.bridgelabz.employeepayrollapp.service;
 import com.bridgelabz.employeepayrollapp.dto.EmployeeDTO;
 import com.bridgelabz.employeepayrollapp.model.Employee;
 import com.bridgelabz.employeepayrollapp.repository.EmployeeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j  // Enables logging using Lombok
 @Service
 public class EmployeeService {
 
@@ -16,26 +18,35 @@ public class EmployeeService {
     private EmployeeRepository employeeRepository;
 
     public List<Employee> getAllEmployees() {
+        log.info("Fetching all employees");
         return employeeRepository.findAll();
     }
 
     public Optional<Employee> getEmployeeById(Long id) {
+        log.info("Fetching employee with ID: {}", id);
         return employeeRepository.findById(id);
     }
 
     public Employee saveEmployee(EmployeeDTO employeeDTO) {
+        log.info("Saving new employee: {}", employeeDTO);
         Employee employee = new Employee(employeeDTO);
         return employeeRepository.save(employee);
     }
 
     public Employee updateEmployee(Long id, EmployeeDTO employeeDTO) {
-        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new RuntimeException("Employee not found"));
+        log.info("Updating employee with ID: {}", id);
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> {
+                    log.error("Employee with ID {} not found", id);
+                    return new RuntimeException("Employee not found");
+                });
         employee.setName(employeeDTO.getName());
         employee.setSalary(employeeDTO.getSalary());
         return employeeRepository.save(employee);
     }
 
     public void deleteEmployee(Long id) {
+        log.warn("Deleting employee with ID: {}", id);
         employeeRepository.deleteById(id);
     }
 }
